38a70a7d1a83eadc986c480b3cc97fb9fea5ab88
Tom Verkon <tverkon@usa.net>
2019-08-08 13:12:04 -0400

    Initial commit

---
+++ src/test/java/com/digits/mybeerservice/MyBeerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digits.mybeerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBeerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
