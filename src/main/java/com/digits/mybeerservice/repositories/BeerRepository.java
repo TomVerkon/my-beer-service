7bcb1ba8f0c38c8a11dc3df7f414ae5061acff57
Tom Verkon <tverkon@usa.net>
2019-08-11 10:39:24 -0400

    Completes and closes #3

---
+++ src/main/java/com/digits/mybeerservice/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package com.digits.mybeerservice.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.digits.mybeerservice.domain.Beer;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
