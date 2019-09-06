c99b2afcaa2b31a9c1fcdd771d402370c799e682
Tom Verkon <tverkon@usa.net>
2019-09-06 17:02:03 -0400

    BeerPagedList closes #8

--- src/main/java/com/digits/mybeerservice/web/model/BeerPagedList.java
+++ src/main/java/com/digits/mybeerservice/web/model/BeerPagedList.java
@@ -3,7 +3,13 @@ package com.digits.mybeerservice.web.model;
 import java.util.List;
 
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
 
@@ -11,14 +17,28 @@ public class BeerPagedList extends PageImpl<BeerDto> {
      * 
      */
     private static final long serialVersionUID = -3292484938112870999L;
-
-    public BeerPagedList(List<BeerDto> content) {
-	super(content);
-	// TODO Auto-generated constructor stub
+    
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+	    @JsonProperty("number") int number,
+	    @JsonProperty("size") int size,
+	    @JsonProperty("totalElement") Long totalElements,
+	    @JsonProperty("pageable") JsonNode pageable,
+	    @JsonProperty("last") boolean last,
+	    @JsonProperty("totalPages") int totalPages,
+	    @JsonProperty("sort") JsonNode sort,
+	    @JsonProperty("first") boolean first,
+	    @JsonProperty("numberOfElements") int numberOfElements
+	    ) {
+	super(content, PageRequest.of(number, size), totalElements);
     }
 
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
 	super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+	super(content);
     }
 
 }
